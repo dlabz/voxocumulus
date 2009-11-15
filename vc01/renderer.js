e5865ddd4405b62eb0c88af189ed268f6208986d
alex <alex@hybodus.local>
2009-11-15 08:59:04 -0700

    Basic skeleton created.

---
+++ vc01/renderer.js
@@ -0,0 +1,8 @@
+function initO3D (clientElements)
+{
+	// Initializes global variables and libraries.
+	var o3dElement = clientElements[0];
+	G.client = o3dElement.client;
+	G.o3d = o3dElement.o3d;
+	G.math = o3djs.math;
+}
