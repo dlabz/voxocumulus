ea8ca3c5528dfbe6603d5a3597df8edcb55132cf
Veljko <designlabz@gmail.com>
2009-11-25 22:42:06 +0100

    made those classes

--- vcExperiments/vox.js
+++ vcExperiments/vox.js
@@ -1,38 +1,60 @@
-var aVoxel = new Object();
+function aVoxel(){
+	this.ARGB = null;//uint
+	this.link = null;//UID
+	this.sides = null;//sides
+	this.A = function(){
+		return (((0xFF000000 & this.ARGB) >> 24) / 255);
+	};
+	this.R = function(){
+		return (((0x00FF0000 & this.ARGB) >> 16) / 255);
+	};
+	this.G = function(){
+		return (((0x0000FF00 & this.ARGB) >> 8) / 255);
+	};
+	this.B = function(){
+		return (((0x000000FF & this.ARGB)) / 255);
+	};
+}
 
 
-var aCube = new Object();
-	aCube.size = 16; //apart from testing with different model sizes, we might allow any cube to be subdevided in more than one power of 2
-	//aCube.UID = newUID(); // TODO: UID 
-	aCube.voxels = new Array(); //3D Array of voxel color.
-	aCube.sides = new Array(); //Array of sides bump maps
+
+
+
+
+function aCube(size){
 	
-
-
+	this.size = size||16; //apart from testing with different model sizes, we might allow any cube to be subdevided in more than one power of 2
+	this.UID = null; // TODO: UID 
+	this.voxels = null; //3D Array of voxel color.
+	this.sides = null; //Array of sides bump maps
+	this.texture = [];
+}		
+//aCube.texture = g_pack.createTexture2D(72, 72, g_o3d.Texture.ARGB8, 1, false);
+      
 
 
 
 //create a random cube
-aCube.randomize = function(){
-	aCube.voxels = new Array();
-	for (var i = 0; i < aCube.size; i++){
-		aCube.voxels[i] = new Array();
-		for (var j = 0; j < aCube.size; j++){
-			aCube.voxels[i][j] = new Array();
-			for (var k = 0; k < aCube.size; k++){
-				if (randRange(1,10)> 0) {
-					aCube.voxels[i][j][k] = new Object();
-					aCube.voxels[i][j][k].sides = new Array();
-					aCube.voxels[i][j][k].ARGB = (randRange(0xFFFEEE8F, 0xFFFFFFFF));
+aCube.prototype.randomize = function(){
+	//alert("randomizing"+ this.size);
+	this.voxels = new Array();
+	for (var i = 0; i < this.size; i++){
+		this.voxels[i] = new Array();
+		for (var j = 0; j < this.size; j++){
+			this.voxels[i][j] = new Array();
+			for (var k = 0; k < this.size; k++){
+				if (randRange(1,10)> 8) {
+					this.voxels[i][j][k] = new aVoxel();
+					this.voxels[i][j][k].ARGB = (randRange(0xFFFEEE8F, 0xFFFFFFFF));
 				} else {
-					aCube.voxels[i][j][k] = undefined;
+					this.voxels[i][j][k] = undefined;
 				};
 			//document.write(vMatrix[i][j][k]+ "<br />");
 			};
 		};
 	};
 //document.write(vMatrix.length);
-	
+
 }; 
 
 
@@ -45,79 +67,79 @@ function randRange(start,end) {
 
 
 
-aCube.rebuild = function(){//a method calculating what sides of each voxel should be rendered. If Turns out to be slow, I'll write a shader to do this
+aCube.prototype.rebuild = function(x,y,z){//a method calculating what sides of each voxel should be rendered. If Turns out to be slow, I'll write a shader to do this
 //alert("rebuilding voxels");
 	if (arguments.length > 0) { 
-		var x = arguments[0];
-		var y = arguments[1];
-		var z = arguments[2];
-		aCube.reskin(x, y, z);
+		//var x = arguments[0];
+		//var y = arguments[1];
+		//var z = arguments[2];
+		this.reskin(x, y, z);
 	}else {
-		for (var x = 0; x < aCube.size; x++) {
-			for (var y = 0; y < aCube.size; y++) {
-				for (var z = 0; z < aCube.size; z++) {
-					aCube.reskin(x, y, z);
+		for (var x = 0; x < this.size; x++) {
+			for (var y = 0; y < this.size; y++) {
+				for (var z = 0; z < this.size; z++) {
+					this.reskin(x, y, z);
 				};
 			};
 		};
 	};
-	//alert("generation done");
+	//alert("rebuilding done");
 };
 
-aCube.reskin = function(x, y, z){ //function that does actual crunching. this one deasn-t handle surrounding objects... might need that
+aCube.prototype.reskin = function(x, y, z){ //function that does actual crunching. this one deasn-t handle surrounding objects... might need that
     var x = arguments[0];
     var y = arguments[1];
     var z = arguments[2];
     //alert(aCube.voxels[x][y][z]);
-    if (aCube.voxels[x][y][z] != null) {
-        aCube.voxels[x][y][z].sides = new Array(true, true, true, true, true, true);
-        if (x < aCube.size - 1) {
-            if (aCube.voxels[x + 1][y][z] == undefined) {
-                aCube.voxels[x][y][z].sides[0] = true;
+    if (this.voxels[x][y][z] != null) {
+        this.voxels[x][y][z].sides = new Array(true, true, true, true, true, true);//use bites to store this...= pow(2,n) 6 bit 
+        if (x < this.size - 1) {
+            if (this.voxels[x + 1][y][z] == undefined) {
+                this.voxels[x][y][z].sides[0] = true;
             }
             else {
-                aCube.voxels[x][y][z].sides[0] = false;
+                this.voxels[x][y][z].sides[0] = false;
             };
                 };
         if (x > 0) {
-            if (aCube.voxels[x - 1][y][z] == undefined) {
-                aCube.voxels[x][y][z].sides[1] = true;
+            if (this.voxels[x - 1][y][z] == undefined) {
+                this.voxels[x][y][z].sides[1] = true;
             }
             else {
-                aCube.voxels[x][y][z].sides[1] = false;
+                this.voxels[x][y][z].sides[1] = false;
             };
                 };
-        if (y < aCube.size - 1) {
-            if (aCube.voxels[x][y + 1][z] == undefined) {
-                aCube.voxels[x][y][z].sides[2] = true;
+        if (y < this.size - 1) {
+            if (this.voxels[x][y + 1][z] == undefined) {
+                this.voxels[x][y][z].sides[2] = true;
             }
             else {
                 //alert("stripp y0");
-                aCube.voxels[x][y][z].sides[2] = false;
+                this.voxels[x][y][z].sides[2] = false;
             };
                 };
         if (y > 0) {
-            if (aCube.voxels[x][y - 1][z] == undefined) {
-                aCube.voxels[x][y][z].sides[3] = true;
+            if (this.voxels[x][y - 1][z] == undefined) {
+                this.voxels[x][y][z].sides[3] = true;
             }
             else {
-                aCube.voxels[x][y][z].sides[3] = false;
+                this.voxels[x][y][z].sides[3] = false;
             };
                 };
-        if (z < aCube.size - 1) {
-            if (aCube.voxels[x][y][z + 1] == undefined) {
-                aCube.voxels[x][y][z].sides[4] = true;
+        if (z < this.size - 1) {
+            if (this.voxels[x][y][z + 1] == undefined) {
+                this.voxels[x][y][z].sides[4] = true;
             }
             else {
-                aCube.voxels[x][y][z].sides[4] = false;
+                this.voxels[x][y][z].sides[4] = false;
             };
                 };
         if (z > 0) {
-            if (aCube.voxels[x][y][z - 1] == undefined) {
-                aCube.voxels[x][y][z].sides[5] = true;
+            if (this.voxels[x][y][z - 1] == undefined) {
+                this.voxels[x][y][z].sides[5] = true;
             }
             else {
-                aCube.voxels[x][y][z].sides[5] = false;
+                this.voxels[x][y][z].sides[5] = false;
             };
                 };
         };
@@ -132,7 +154,7 @@ aCube.reskin = function(x, y, z){ //function that does actual crunching. this on
 
 
 //this doesn't work
-aCube.createVoxelVertices = function(size, x,y,z) {
+aCube.prototype.createVoxelVertices = function(size, x,y,z) {
   var k = size / 2;
 	//var x = x;
 	//var y = y;
@@ -176,7 +198,7 @@ aCube.createVoxelVertices = function(size, x,y,z) {
   for (var f = 0; f < 6; ++f) {
   	
   	//if (aCube.voxels[x][y][z].sides[f] == true){//here is where the magic happens... i read my array and decide whatsurfaces to draw on the voxel
-			if (aCube.voxels[x][y][z].sides[f] == true) {
+			if (this.voxels[x][y][z].sides[f] == true) {
 				var faceIndices = o3djs.primitives.CUBE_FACE_INDICES_[f];
 			
 				for (var v = 0; v < 4; ++v) {
@@ -211,17 +233,17 @@ aCube.createVoxelVertices = function(size, x,y,z) {
   
 };
 
-aCube.createVoxel = function(pack,
+aCube.prototype.createVoxel = function(pack,
                                        material,
                                        size,
                                        x,y,z) {
 	
 	//alert("new voxel");
-  var vertexInfo = aCube.createVoxelVertices(size, x,y,z);
+  var vertexInfo = this.createVoxelVertices(size, x,y,z);
   return vertexInfo.createShape(pack, material);
 };
 
-aCube.makeVoxel = function(pack,
+aCube.prototype.makeVoxel = function(pack,
                                        material,
                                        size,
                                        x,y,z) {
@@ -236,6 +258,6 @@ aCube.makeVoxel = function(pack,
                                         //opt_matrix
 };
 	//alert("new voxel");
-  var vertexInfo = aCube.createVoxelVertices(size, x,y,z);
+  var vertexInfo = this.createVoxelVertices(size, x,y,z);
   return vertexInfo.createShape(pack, material);
 };
